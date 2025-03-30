8d679c9efb305de8f1a699a2014af714cb61ac3c
Davidhero <owogogahhero@outlook.com>
2025-03-30 23:43:53 +0100

    i don't think I know what I am doing

---
+++ cpp_cross/libs/DemoLib/DemoLib.cpp
@@ -0,0 +1,5 @@
+#include "DemoLib.h"
+
+int DemoLib::getNumber() { return this->m_number; }
+
+void DemoLib::setNumber(int in_number) { this->m_number = in_number; }
---
+++ cpp_cross/libs/DemoLib/DemoLib.h
@@ -0,0 +1,9 @@
+#pragma once
+class DemoLib {
+private:
+  int m_number;
+
+public:
+  int getNumber();
+  void setNumber(int in_number);
+};
