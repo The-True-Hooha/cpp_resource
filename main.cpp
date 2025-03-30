9df2c473f4676069b6fecb338718045632ad37c0
Davidhero <owogogahhero@outlook.com>
2025-03-30 23:35:00 +0100

    refactor: new changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
-#include <format>
+#include <string>
+// #include <format>
+
+namespace trial {
+    std::string yse = "this is another namespace";
+}
+
+namespace real {
+    int x= 33;
+
+}
 
 int main() {
+    // writing comments in c++
+    /*
+    multi line comment
+    */
+    float d;
+    d = 10.3;
+    double jj;
+    jj = 88.4;
     std::string name = "World";
-    std::cout << std::format("Hello, {}!\n", name);
+    // std::cout << std::format("Hello, {}!\n", name);
+    // std::cout << "I am writing for me" << std::endl;
+    int f;
+    f=3;
+    // int number = 3;
+
+    // std::cout << number;
+    bool boy = true;
+
+    std::string people;
+    people = "computer science is the real deal\n";
+    // std::cout << people << name << std::endl;
+
+    const std::string MEK = "this is a one shot attempt";
+
     return 0;
+
+    // setting variables
+    // 1. declaration and by assignments
 }
